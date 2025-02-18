f8fd36d434f96c07c5fa309c91bed6c371ce122f
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-02-18 15:07:26 +0300

    начата страница iw28s

---
+++ js/swiper-goods.js
@@ -0,0 +1,37 @@
+var swiper2 = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  direction: "vertical",
+  freeMode: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    426: {
+      direction: "horizontal",
+    },
+    550: {
+      direction: "vertical",
+    },
+  },
+});
+
+var swiper = new Swiper(".goodSwiper", {
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1301: {
+      slidesPerView: 1,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  thumbs: {
+    swiper: swiper2,
+  },
+});
