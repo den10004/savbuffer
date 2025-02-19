0694e65aac5163dd67115b243f8b15816db519ba
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-02-19 15:49:39 +0300

    якорные ссылки

--- js/main.js
+++ js/main.js
@@ -233,3 +233,21 @@ function sendForm(form) {
     }
   });
 }
+
+//плавный скролл
+
+/*
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+*/
